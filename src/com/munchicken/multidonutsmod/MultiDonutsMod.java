834ad5a873c9484ae8ba862d7905267c9526e202
Sarah Pierce <hacknsnack@yahoo.com>
2014-12-23 01:19:58 -0600

    Setup project and created basic mod files

---
+++ src/com/munchicken/multidonutsmod/MultiDonutsMod.java
@@ -0,0 +1,16 @@
+package com.munchicken.multidonutsmod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import com.munchicken.multidonutsmod.help.*;
+
+@Mod(modid = Reference.MODID, version = Reference.VERSION)
+public class MultiDonutsMod {
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
---
+++ src/com/munchicken/multidonutsmod/help/Reference.java
@@ -0,0 +1,8 @@
+package com.munchicken.multidonutsmod.help;
+
+public class Reference {
+	
+	public static final String MODID = "munchicken_multidonutsmod";
+	public static final String VERSION = "1.7.2-0.1";
+
+}
