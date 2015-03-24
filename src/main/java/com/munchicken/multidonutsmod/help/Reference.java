2aaedeabdbd28f648ba8e6f0794e6263ecfbd78b
Sarah Pierce <spierce@austin.rr.com>
2015-03-24 02:14:58 -0500

    Changed to Maven convention for directory structure. Changes in dev environment:  Switched from Forge 1.7.10 back to Forge 1.7.2, Switched from Java 8 to 7, Switched from Github client to SourceTree, and Switched from Eclipse to IntelliJ.

--- src/main/java/com/munchicken/multidonutsmod/help/Reference.java
+++ src/main/java/com/munchicken/multidonutsmod/help/Reference.java
@@ -3,6 +3,6 @@ package com.munchicken.multidonutsmod.help;
 public class Reference {
 	
 	public static final String MODID = "munchicken_multidonutsmod";
-	public static final String VERSION = "1.7.10-0.1";
+	public static final String VERSION = "1.7.2-0.1";
 
 }
